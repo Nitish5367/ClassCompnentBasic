d08728620aa6feb2da7add3f049613c31cef7876
Nitish5367 <95980829+Nitish5367@users.noreply.github.com>
2023-12-29 15:57:37 +0530

    ClassCompnent

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+class App extends Component{
+  render()
+    {
+      return(<div>
+        <h1>Class Comp</h1>
+        <p>Just demo</p>
+        </div>)
+    }
+
+   
+}
+export default App
